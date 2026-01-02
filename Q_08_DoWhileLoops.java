70e6afb73423a49a99467e827b26ccd6537c8669
RG <rgirigeek@gmail.com>
2026-01-02 08:35:14 +0530

    Q_08_DoWhileLoops

---
+++ Q_08_DoWhileLoops.java
@@ -0,0 +1,10 @@
+public class Q_08_DoWhileLoops {
+    public static void main(String[] args) {//Do-While Loop Runs At least One Time;
+    int n = 10;
+    int i = 1;
+    do{
+         System.out.println("Hey,this is the " + i + "'th iteration");
+         i++;
+    }while(i<=n);
+}
+}
