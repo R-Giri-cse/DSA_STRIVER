cb12e8b0d81cd7cf6549c5a0a47b921693cbef0d
RG <rgirigeek@gmail.com>
2026-01-02 08:34:45 +0530

    Q_07_WhileLoops

---
+++ Q_07_WhileLoops.java
@@ -0,0 +1,12 @@
+public class Q_07_WhileLoops {
+    public static void main(String[] args) {
+        int n = 10;
+        int i = 1;
+        while(i<=n){
+             System.out.println("Hey,this is the " + i + "'th iteration");
+             i++;
+        }
+       
+    }
+}
+    
