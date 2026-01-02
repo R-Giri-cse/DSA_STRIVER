02327c223ca50e925372f66d63de95c2640bdb41
RG <rgirigeek@gmail.com>
2026-01-02 08:34:18 +0530

    Q_06_ForLoops

---
+++ Q_06_ForLoops.java
@@ -0,0 +1,8 @@
+public class Q_06_ForLoops {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hey, Striver, this is the " + i + "'th iteration");
+        }
+    }
+}
+
